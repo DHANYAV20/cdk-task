e55879163e6903217a0bdaf18cdc58020dc43aa4
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-02-23 18:36:05 +0530

    add config file and cloudwatch

--- custom_vpc/custom_vpc_stack.py
+++ custom_vpc/custom_vpc_stack.py
@@ -18,86 +18,102 @@ class CustomVpcStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         vpc = ec2.Vpc(
-                self, "TaskVpc011",
-                cidr="10.20.0.0/16",
+                self, config["vpc"]["id"],
+                cidr= config["vpc"]["cidr"],
                 max_azs=1,subnet_configuration=[])
-                #nat_gateways=1,
-                #internet_gateway_id="internetGatewayId",
-                #nat_gateway_provider=ec2.NatProvider.gateway(),
-                
-        elastic_ip = ec2.CfnEIP(self, 
-                                "EIP",
-                                tags=[CfnTag(key="Name", value="elasticIp")]
-                                )
-    
-        cfn_internet_gateway = ec2.CfnInternetGateway(self, 
-                                    "IGWPublicSubnet01")
-        cfn_vPCGateway_attachment = ec2.CfnVPCGatewayAttachment(self, 
-                                        "IGWPublicSubnet01vpcattachement", 
-                                        vpc_id=vpc.vpc_id, 
-                                        internet_gateway_id=cfn_internet_gateway.attr_internet_gateway_id)
+        cfn_internet_gateway = ec2.CfnInternetGateway(self,config["igw"]["id"],tags=[CfnTag(key="Name",value=config["igw"]["name"])])
+        cfn_vPCGateway_attachment = ec2.CfnVPCGatewayAttachment(self,config["igwAttachment"]["id"],vpc_id=vpc.vpc_id,internet_gateway_id= cfn_internet_gateway.attr_internet_gateway_id)
 
-        publicSubnet= ec2.Subnet(self,
-                             id="PublicSubnet01", 
-                             availability_zone=vpc.availability_zones[0], 
-                             cidr_block="10.20.1.0/24",
-                             vpc_id=vpc.vpc_id , 
-                             map_public_ip_on_launch=True)
-        privateSubnet= ec2.Subnet(self, 
-                            id="PrivateSubnet01", 
+
+        cfn_elasticIp = ec2.CfnEIP(self,id= config["elasticIp"]["id"],
+                            tags=[CfnTag(key="Name",
+                            value=config["elasticIp"]["name"])])
+        
+
+        publicSubnet = ec2.Subnet(self,
+                            id=config["publicSubnet"]["id"],
                             availability_zone=vpc.availability_zones[0],
-                            cidr_block="10.20.2.0/24", 
-                            vpc_id=vpc.vpc_id ,
-                            map_public_ip_on_launch=False)
-        cfn_nat_gateway= ec2.CfnNatGateway(self,
-                                 "NatGatewayPrivateSubnet01", 
-                                 subnet_id=publicSubnet.subnet_id, 
-                                 connectivity_type="public",
-                                 allocation_id=elastic_ip.attr_allocation_id,
-                                 tags=[CfnTag(key="Name", value="NatGatewayPrivateSubnet01")]
-                                 )
-        cfn_route_internet_gateway = ec2.CfnRoute(self,
-                                        id = "publicSubnetgateway", 
-                                        route_table_id=publicSubnet.route_table.route_table_id,
-                                        destination_cidr_block="0.0.0.0/0",
-                                        gateway_id=cfn_internet_gateway.attr_internet_gateway_id
-                                        
-                                        )
-        cfn_route_nat_gateway = ec2.CfnRoute(self, 
-                                    id = "privateSubnetgateway", 
-                                    route_table_id=privateSubnet.route_table.route_table_id,
-                                    destination_cidr_block="0.0.0.0/0",
-                                    nat_gateway_id=cfn_nat_gateway.ref)
-        sec_group = ec2.SecurityGroup(self,
-                        id="sg-securitygroup01",
-                        security_group_name="CDKSecurity",
-                        description="sec-group-allow-ssh",
-                        vpc=vpc,
-                        allow_all_outbound=True
-        )
-        sec_group.add_ingress_rule(peer=ec2.Peer.any_ipv4(),
-                    description="Allow SSH connection", 
-                    connection=ec2.Port.tcp(22))
+                            cidr_block=config["publicSubnet"]["cidr"],
+                            vpc_id=vpc.vpc_id,
+                            map_public_ip_on_launch=True,
 
-        # define a new ec2 instance
-        publicSubnetInstance = ec2.Instance(self,
-                        "MyPublicInstance",
-                        instance_name="MyInstance1",
-                        machine_image=ec2.MachineImage.latest_amazon_linux(),
-                        instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2,ec2.InstanceSize.MICRO),
-                        vpc=vpc,
-                        key_name="DhanyaKey",
-                        vpc_subnets=ec2.SubnetSelection(subnets=[publicSubnet]),
-                        security_group=sec_group
-                                             
-        )
-        privateSubnetInstance = ec2.Instance(self, 
-                        "MyPrivateInstance",
-                        instance_name="MyInstance2",
-                        instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2,ec2.InstanceSize.MICRO),
-                        machine_image=ec2.MachineImage.latest_amazon_linux(),
-                        vpc=vpc,
-                        key_name="DhanyaKey",
-                        vpc_subnets=ec2.SubnetSelection(subnets=[privateSubnet]),
-                        security_group=sec_group
                         )
+
+        privateSubnet = ec2.Subnet(self,
+                            id=config["privateSubnet"]["id"],
+                            availability_zone=vpc.availability_zones[0],
+                            cidr_block=config["privateSubnet"]["cidr"],
+                            vpc_id=vpc.vpc_id,
+                            map_public_ip_on_launch=False
+                        )
+       
+
+        cfn_nat_gateway = ec2.CfnNatGateway(self,
+                            id=config["natgw"]["id"],
+                            subnet_id=publicSubnet.subnet_id,
+                            connectivity_type="public",
+                            allocation_id= cfn_elasticIp.attr_allocation_id,
+                            tags=[CfnTag(
+                                    key="Name",
+                                    value=config["natgw"]["name"]
+                                    )
+                                ]
+                        )
+        cfn_route_nat_gateway = ec2.CfnRoute(self,
+                                    id=config["natgwRoute"]["id"],
+                                    route_table_id=privateSubnet.route_table.route_table_id,
+                                    destination_cidr_block=config["natgwRoute"]["cidr"],
+                                    nat_gateway_id=cfn_nat_gateway.ref)
+
+        cfn_route_internet_gateway = ec2.CfnRoute(self,
+                                    id=config["igwRoute"]["id"],
+                                    route_table_id=publicSubnet.route_table.route_table_id,
+                                    destination_cidr_block=config["igwRoute"]["cidr"],
+                                    gateway_id=cfn_internet_gateway.attr_internet_gateway_id)
+        
+        securityGroup = ec2.SecurityGroup(self,
+                            id=config["securityGroup"]["id"],
+                            vpc = vpc,
+                            security_group_name=config["securityGroup"]["name"],
+                            description=config["securityGroup"]["description"])
+        securityGroup.add_ingress_rule(peer=config["inboundRule"]["peer"],
+                                        connection=config["inboundRule"]["connection"],
+                                        description=config["inboundRule"]["description"])
+        
+        Tag.of(securityGroup).add("Name",config["securityGroup"]["displayName"])
+        publicSubnetInstance = ec2.Instance(self,
+                                config["publicInstance"]["id"], 
+                                instance_type= config["publicInstance"]["type"], 
+                                machine_image= config["publicInstance"]["image"],
+                                vpc=vpc,
+                                security_group= securityGroup,
+                                key_name=config["publicInstance"]["keyName"],
+                                vpc_subnets=ec2.SubnetSelection(subnets=[publicSubnet],
+                                
+                                )
+                                ) 
+        privateSubnetInstance = ec2.Instance(self,
+                                config["privateInstance"]["id"], 
+                                instance_type= config["privateInstance"]["type"], 
+                                machine_image= config["privateInstance"]["image"],
+                                vpc=vpc,
+                                security_group= securityGroup,
+                                key_name=config["privateInstance"]["keyName"],
+                                vpc_subnets=ec2.SubnetSelection(subnets=[privateSubnet])
+                                ) 
+        metric = cloudwatch.Metric(
+                                    namespace=config["metric"]["namespace"],
+                                    metric_name=config["metric"]["metric_name"],
+                                    period=Duration.minutes(1),
+                                    dimensions_map={
+                                                "Name" : "publicInstance",
+                                                "Value" : "publicInstance"
+                                            }
+                                    )
+        alarm = cloudwatch.Alarm(self, "Alarm",
+                                    metric=metric,
+                                    threshold=config["alarm"]["threshold"],
+                                    evaluation_periods=config["alarm"]["evaluation_periods"],
+                                    datapoints_to_alarm=config["alarm"]["datapoints_to_alarm"]
+                                    
+                                )
