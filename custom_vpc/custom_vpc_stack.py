d5b302fe66efc3500744462dea584b1769d77788
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-03-03 22:22:25 +0530

    added rds and elastic load balancer

--- custom_vpc/custom_vpc_stack.py
+++ custom_vpc/custom_vpc_stack.py
@@ -101,19 +101,83 @@ class CustomVpcStack(Stack):
                                 key_name=config["privateInstance"]["keyName"],
                                 vpc_subnets=ec2.SubnetSelection(subnets=[privateSubnet])
                                 ) 
+        Instance_elasticIp = ec2.CfnEIP(self,id= "InstanceEIP",
+                                        tags=[CfnTag(key="Name",value="InstanceEIP")],
+                                        instance_id=publicSubnetInstance.instance_id)
         metric = cloudwatch.Metric(
                                     namespace=config["metric"]["namespace"],
                                     metric_name=config["metric"]["metric_name"],
-                                    period=Duration.minutes(1),
+                                    period=Duration.minutes(5),
                                     dimensions_map={
-                                                "Name" : "publicInstance",
-                                                "Value" : "publicInstance"
-                                            }
+                                                "InstanceId" : publicSubnetInstance.instance_id 
+                                            },
+                                    statistic=config["metric"]["statistic"],
+                                    unit=cloudwatch.Unit.PERCENT,
+                                    label="CPU Utilization metric"
+
                                     )
+        #Low CPU alarm
         alarm = cloudwatch.Alarm(self, "Alarm",
                                     metric=metric,
+                                    alarm_name=config["alarm"]["alarm_name"],
+                                    alarm_description=config["alarm"]["alarm_description"],
                                     threshold=config["alarm"]["threshold"],
                                     evaluation_periods=config["alarm"]["evaluation_periods"],
-                                    datapoints_to_alarm=config["alarm"]["datapoints_to_alarm"]
+                                    datapoints_to_alarm=config["alarm"]["datapoints_to_alarm"],
+                                    actions_enabled=True,
+                                    comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_OR_EQUAL_TO_THRESHOLD,
+                                    
+                                    treat_missing_data=cloudwatch.TreatMissingData.NOT_BREACHING
                                     
                                 )
+        #Create SNS Topic 
+        sns = _sns.Topic(self, "KonstoneTeam", display_name=config["sns"]["display_name"], topic_name=config["sns"]["topic_name"])
+        
+        #Add subscription to SNS topic
+        sns.add_subscription(_subs.EmailSubscription("dhanya.v@cloudplusinfotech.com"))
+        
+
+        #Inform SNS on ec2 alarm state
+        alarm.add_alarm_action=(_cloudwatch_actions.SnsAction(sns))
+
+        #konstone_fn = _lambda.Function(self, )
+
+        #lambdaIAM = _iam.Role(self ,"MyRole" , )
+
+        #
+        vpc_subnet_selection = ec2.SubnetSelection(subnets = [publicSubnet, privateSubnet])
+
+        #create application load balancer
+        alb = _elbv2.ApplicationLoadBalancer(self, 
+                        config["alb"]["id"],
+                        vpc=vpc,
+                        internet_facing=config["alb"]["internet_facing"], 
+                        load_balancer_name=config["alb"]["load_balancer_name"],
+                        vpc_subnets=vpc_subnet_selection)
+
+        #create rds database
+        rds = _rds.DatabaseInstance(self, 
+                    config["rds"]["id"],
+                    vpc=vpc,
+                    database_name=config["rds"]["database_name"],
+                    allocated_storage=config["rds"]["allocated_storage"], 
+                    port=3308, 
+                    multi_az=config["rds"]["multi_az"], 
+                    engine=_rds.DatabaseInstanceEngine.MYSQL , 
+                    cloudwatch_logs_exports=["audit","error", "general", "slowquery"], 
+                    instance_identifier="publicSubnetInstance",
+                    removal_policy=RemovalPolicy.DESTROY, 
+                    deletion_protection=config["rds"]["deletion_protection"], 
+                    delete_automated_backups=config["rds"]["delete_automated_backups"], 
+                    backup_retention=Duration.days(7),
+                    vpc_subnets= vpc_subnet_selection,
+                    instance_type=config["rds"]["instance_type"]
+        )
+        
+        
+
+       
+        
+                                    
+                    
+        
